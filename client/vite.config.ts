14b6e12f667d4ff777757ab01c0fb39d069d9ef6
sheddy-bx <shadrack@bx.studio>
2025-02-07 11:13:01 +0100

    feat: enhance Vite configuration with dynamic entry points and additional types

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,35 @@
-export default {
+import { defineConfig } from "vite";
+
+import { glob } from "glob";
+import path from "path";
+
+export default defineConfig({
   server: {
     cors: {
       origin: "*", // Allow all origins
     },
   },
-};
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+        ...Object.fromEntries(
+          glob.sync("src/**/*.ts").map((file) => [
+            // Get the file name without extension as the entry key
+            path.basename(file, ".ts"),
+            // Get the absolute path as the entry value
+            path.resolve(file),
+          ])
+        ),
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "chunks/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
+  },
+});
