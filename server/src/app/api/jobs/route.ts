6e4d9271af7c0a5c222e27bbcd7b7e51c14d524f
sheddy-bx <shadrack@bx.studio>
2025-02-13 21:03:01 +0100

    feat: add last modified date to job details API response

--- server/src/app/api/jobs/route.ts
+++ server/src/app/api/jobs/route.ts
@@ -17,7 +17,7 @@ export async function GET() {
     baseURL.searchParams.set("query", "(isOpen:1)");
     baseURL.searchParams.set(
       "fields",
-      "benefits,externalCategoryID,publicDescription,willSponsor,dateEnd,salary,address(address1,city,state,zip,countryID),title,dateAdded,isDeleted,dateLastPublished,isPublic,id,yearsRequired,customText2,bonusPackage,employmentType,travelRequirements,payRate,publishedZip,salaryUnit,willRelocate,responseUser(id,firstName,lastName),isOpen,publishedCategory(id,name),startDate,_score,isWorkFromHome"
+      "benefits,externalCategoryID,publicDescription,willSponsor,dateEnd,salary,address(address1,city,state,zip,countryID),title,dateAdded,isDeleted,dateLastPublished,isPublic,id,yearsRequired,customText2,bonusPackage,employmentType,travelRequirements,payRate,publishedZip,salaryUnit,willRelocate,responseUser(id,firstName,lastName),isOpen,publishedCategory(id,name),startDate,_score,isWorkFromHome,dateLastModified"
     );
     // baseURL.searchParams.set("fields", "*");
     baseURL.searchParams.set("count", "200");
